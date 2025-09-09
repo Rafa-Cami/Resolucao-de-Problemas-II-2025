7a4c22cc48f254ad92a3684a66e851e792a82233
Rafa-Cami <106712987+Rafa-Cami@users.noreply.github.com>
2025-09-08 21:51:59 -0300

    FUNCIONAUR

--- analise.py
+++ analise.py
@@ -1,7 +1,3 @@
-import pandas as pd
-
-print("Análise de Dados Iniciada")
-
 # FILTRAGEM INICIAL: 
 # FUNÇÃO: EDUCAÇÃO / SUBFUNÇÃO DO GOVERNO: ENSINO SUPERIOR
 # DE 2017 A 2024
@@ -12,22 +8,35 @@ print("Análise de Dados Iniciada")
 # agrupar universidade para somar investimentos por mês > mais tarde com o grupo
 # tirar coluna K (UF PT) - OKAY
 
-df_dados = pd.read_csv('dados.csv', delimiter=";")
+import pandas as pd
+
+print("Análise de Dados Iniciada")
+
+df_dados = pd.read_csv(
+    '2017-2024.csv',
+    delimiter=";",
+    decimal=",",        # decimal é vírgula
+    thousands=".",      # separador de milhar é ponto
+    encoding="latin1"   # corrige acentuação
+)
+
 print("CSV carregado com sucesso")
 
-# Sem coluna K
+# Remover coluna indesejada
 df_dados.drop('UF PT', axis=1, inplace=True)
 
-# Adicionando coluna Período
+# Criar coluna Período
 df_dados.loc[(df_dados['Ano Lançamento'] >= 2017) & (df_dados['Ano Lançamento'] < 2020), 'Período'] = 'PRÉ-PANDEMIA'
 df_dados.loc[(df_dados['Ano Lançamento'] >= 2020) & (df_dados['Ano Lançamento'] <= 2021), 'Período'] = 'PANDEMIA'
 df_dados.loc[(df_dados['Ano Lançamento'] > 2021), 'Período'] = 'PÓS-PANDEMIA'
 
+# Filtrar unidade orçamentária
+df_dados = df_dados[
+    (df_dados['Unidade Orçamentária'].str.contains('UNIVERSIDADE')) |
+    (df_dados['Unidade Orçamentária'].str.contains('UNIV'))
+]
 
-# Filtrando coluna de unidade orçamentária
-df_dados = df_dados[(df_dados['Unidade Orçamentária'].str.contains('UNIVERSIDADE'))
-                    | (df_dados['Unidade Orçamentária'].str.contains('UNIV'))]
+# Salvar de volta
+df_dados.to_csv("dados.csv", index=False, sep=";", decimal=",", encoding="latin1")
 
-df_dados.to_csv("dados.csv", index=False, sep=";")
 print("CSV atualizado e salvo")
-
